59ff2e04b74710a250da36993774f1f316d37b91
MichaelSparks004 <chiazokaobasi03@gmail.com>
2022-10-08 16:59:33 -0700

    monty pair project files

---
+++ nop.c
@@ -0,0 +1,16 @@
+es (13 sloc)  211 Bytes
+#include "monty.h"
+
+/**
+ * nop -  does nothing
+ * @stack: doesnt matter
+ * @line_cnt : for nothing
+ *
+ * Return: NOTHING
+ */
+
+void nop(stack_t **stack, unsigned int line_cnt)
+{
+	(void) stack;
+	(void) line_cnt;
+}
